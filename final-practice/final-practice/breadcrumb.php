e60eecd6c68be2a0b6ae9af8def49c6705f9efac
design0011 <138834827+design0011@users.noreply.github.com>
2024-01-05 14:50:39 +0900

    Delete final-practice/final-practice directory

--- final-practice/final-practice/breadcrumb.php
+++
@@ -1,7 +0,0 @@
-<div class="breadcrumb">
-  <?php
-    if ( function_exists( 'bcn_display' ) ) {
-      bcn_display();
-    }
-  ?>
-</div>
--- final-practice/final-practice/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-  // すべてのアイキャッチ画像の有効化
-  add_theme_support('post-thumbnails');
-?>
